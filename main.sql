05ff71a171da513eaf9f8525059f7227d31f8899
foaf20@bath.ac.uk <foaf20@bath.ac.uk>
2025-05-26 21:10:49 +0000

    changes made

--- main.sql
+++ main.sql
@@ -1,27 +1,27 @@
 
 CREATE DATABASE animals;
-CREATE TABLE animals.pets (petID INT PRIMARY KEY, name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups INT, birth DATE, death DATE);
+CREATE TABLE pets (petID INT PRIMARY KEY, name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups INT, birth DATE, death DATE);
 
-INSERT INTO animals.pets (petID, name, owner, species, sex, checkups, birth, death)
+INSERT INTO pets (petID, name, owner, species, sex, checkups, birth, death)
 VALUES (1, Bruno, Feyi, Dog, F, 5, 2021-05-21, 2025-05-26);
 
-INSERT INTO animlas.pets (petID, name, owner, species, sex, checkups, birth, death)
+INSERT INTO pets (petID, name, owner, species, sex, checkups, birth, death)
 VALUES (2, Athena, Jack, Cat, M, 2, 2021-01-16, 2024-02-14);
 
-INSERT INTO animals.pets (petID, name, owner, species, sex, checkups, birth, death)
+INSERT INTO pets (petID, name, owner, species, sex, checkups, birth, death)
 VALUES (3, Peter, Harriet, Parrot, M, 2, 2021-01-31, 2024-12-14);
 
-INSERT INTO animals.pets(petID, name, owner, species, sex, checkups, birth, death)
+INSERT INTO pets(petID, name, owner, species, sex, checkups, birth, death)
 VALUES (4, Bella, Ray, Goldfish, F, 1, 2022-03-01, 2024-03-01);
 --.SCHEMA
 --.TABLE
 -- SQLBook: Code
-SELECT * FROM animals.pets;
+SELECT * FROM pets;
 -- SQLBook: Code
 
 -- SQLBook: Code
-SELECT * FROM animals.pets WHERE species = 'Dog';
-SHOW TABLE animals.pets;
+SELECT * FROM pets WHERE species = 'Dog';
+SHOW TABLE pets;
 --.schema 
 --.table
 -- SQLBook: Code
