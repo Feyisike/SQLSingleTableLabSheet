6d6dbadc090fc70b4107b6f813baa99d82ba220b
foaf20@bath.ac.uk <foaf20@bath.ac.uk>
2025-05-26 08:24:44 +0000

    changes

--- main.sql
+++ main.sql
@@ -1,7 +1,6 @@
+-- SQLBook: Code
 
 -- Single Table Lab Sheet
 
-CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups SMALLINT UNSIGNED, birth DATE, death DATE);
+CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups INT, birth DATE, death DATE);
 
-. schema
-. table
