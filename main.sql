fa060cc2f3ddc0c25d049e729b494f2f45042c2d
foaf20@bath.ac.uk <foaf20@bath.ac.uk>
2025-05-26 11:30:05 +0000

    changing

--- main.sql
+++ main.sql
@@ -4,3 +4,10 @@
 
 CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups INT, birth DATE, death DATE);
 
+--.SCHEMA
+--.TABLE
+-- SQLBook: Code
+
+-- SQLBook: Code
+
+-- SQLBook: Code
