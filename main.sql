765b5b29972be28995bc5621fd006a6730e1af99
foaf20@bath.ac.uk <foaf20@bath.ac.uk>
2025-05-26 19:53:04 +0000

    changed

--- main.sql
+++ main.sql
@@ -1,21 +1,21 @@
 
-CREATE DATABASE testdB;
-CREATE TABLE testdB.pets (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups INT, birth DATE, death DATE);
+CREATE DATABASE animals;
+CREATE TABLE pets (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups INT, birth DATE, death DATE);
 
-INSERT INTO testdB.pets (name, owner, species, sex, checkups, birth, death)
-VALUES (Bruno, Feyi, Dog, F, 5, 20/5/2021, 26/05/2025);
+INSERT INTO pets (name, owner, species, sex, checkups, birth, death)
+VALUES (Bruno, Feyi, Dog, F, 5, 2021-05-21, 2025-05-26);
 
-INSERT INTO testdB.pets (name, owner, species, sex, checkups, birth, death)
-VALUES (Athena, Jack, Cat, M, 2, 16/1/2021, 14/2/2024);
+INSERT INTO pets (name, owner, species, sex, checkups, birth, death)
+VALUES (Athena, Jack, Cat, M, 2, 2021-01-16, 2024-02-14);
 
-INSERT INTO testdB.pets (name, owner, species, sex, checkups, birth, death)
-VALUES (Peter, Harriet, Parrot, M, 2, 31/1/2021, 14/12/2024);
+INSERT INTO pets (name, owner, species, sex, checkups, birth, death)
+VALUES (Peter, Harriet, Parrot, M, 2, 2021-01-31, 2024-12-14);
 -- SQLBook: Code
-SELECT * FROM testdB.pets;
+SELECT * FROM pets;
 -- SQLBook: Code
 
 -- SQLBook: Code
-SELECT * FROM testdB.pets WHERE species = 'Dog';
+SELECT * FROM pets WHERE species = 'Dog';
 .schema 
 .table
 -- SQLBook: Code
