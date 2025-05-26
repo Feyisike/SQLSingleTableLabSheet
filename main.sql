2852eaa7755b5d0ff6748fd4bf0aca2feea9b764
foaf20@bath.ac.uk <foaf20@bath.ac.uk>
2025-05-26 13:11:20 +0000

    chanes

--- main.sql
+++ main.sql
@@ -8,18 +8,20 @@
 --.TABLE
 
 --CREATE DATABASE testdB;
-CREATE TABLE pets (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups INT, birth DATE, death DATE);
+CREATE TABLE testdB.pets (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups INT, birth DATE, death DATE);
 
-INSERT INTO pets (name, owner, species, sex, checkups, birth,death)
+INSERT INTO testdB.pets (name, owner, species, sex, checkups, birth, death)
 VALUES (Bruno, Feyi, Dog, F, 5, 20/5/2021, 26/05/2025);
 
-INSERT INTO pets (name, owner, species, sex, checkups, birth,death)
+INSERT INTO testdB.pets (name, owner, species, sex, checkups, birth, death)
 VALUES (Athena, Jack, Cat, M, 2, 16/1/2021, 14/2/2024);
 
-INSERT INTO pets (name, owner, species, sex, checkups, birth,death)
+INSERT INTO testdB.pets (name, owner, species, sex, checkups, birth, death)
 VALUES (Peter, Harriet, Parrot, M, 2, 31/1/2021, 14/12/2024);
 -- SQLBook: Code
-SELECT * FROM pets;
+SELECT * FROM testdB.pets;
 -- SQLBook: Code
 
 -- SQLBook: Code
+SELECT * FROM testdB.pets WHERE species = 'Dog';
+SCHEMA pets;
