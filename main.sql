f7f9b4f1d374e6f88798a7f6d1ab7b079c0297ec
foaf20@bath.ac.uk <foaf20@bath.ac.uk>
2025-05-28 04:18:01 +0000

    changes

--- main.sql
+++ main.sql
@@ -1,5 +1,5 @@
 
-CREATE DATABASE animals;
+CREATE DATABASE pets_db;
 CREATE TABLE pets (petID INT PRIMARY KEY, name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups INT, birth DATE, death DATE);
 
 INSERT INTO pets (petID, name, owner, species, sex, checkups, birth, death)
@@ -11,7 +11,7 @@ VALUES (2, Athena, Jack, Cat, M, 2, 2021-01-16, 2024-02-14);
 INSERT INTO pets (petID, name, owner, species, sex, checkups, birth, death)
 VALUES (3, Peter, Harriet, Parrot, M, 2, 2021-01-31, 2024-12-14);
 
-INSERT INTO pets(petID, name, owner, species, sex, checkups, birth, death)
+INSERT INTO pets (petID, name, owner, species, sex, checkups, birth, death)
 VALUES (4, Bella, Ray, Goldfish, F, 1, 2022-03-01, 2024-03-01);
 --.schema pets
 --.TABLE
