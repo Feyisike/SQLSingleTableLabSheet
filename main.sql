aadb9bac10f8b8b9ed487418fcd22967de359aa5
foaf20@bath.ac.uk <foaf20@bath.ac.uk>
2025-05-28 04:31:40 +0000

    c

--- main.sql
+++ main.sql
@@ -1,7 +1,7 @@
 -- Active: 1748403138611@@127.0.0.1@3306
 
 CREATE DATABASE pets_db;
-CREATE TABLE pets (petID INT PRIMARY KEY, name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups INT, birth DATE, death DATE);
+CREATE TABLE pets (petID INT PRIMARY KEY, name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups INTEGER, birth DATE, death DATE);
 
 INSERT INTO pets (petID, name, owner, species, sex, checkups, birth, death)
 VALUES (1, Bruno, Feyi, Dog, F, 5, 2021-05-21, 2025-05-26);
